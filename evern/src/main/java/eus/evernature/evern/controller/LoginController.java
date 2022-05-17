776d0b253ce9af8dc59211244df2cddbbba795fb
iarcos <izan.arcos@alumni.mondragon.edu>
2022-05-17 10:11:47 +0200

    Home controller and login controller created

--- evern/src/main/java/eus/evernature/evern/controller/LoginController.java
+++ evern/src/main/java/eus/evernature/evern/controller/LoginController.java
@@ -5,11 +5,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/login")
 public class LoginController {
 
-  @GetMapping("/login")
+  @GetMapping
   public String login(HttpServletRequest request, HttpServletResponse response) {
     return "login";
   }
