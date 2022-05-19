71bec0f9a15e78ba95a87125e9c14385e75b793c
Aritz Domaika <aritz.domaika@alumni.mondragon.edu>
2022-05-19 11:15:55 +0200

    url service implementarion

--- evern/src/main/java/eus/evernature/evern/service/urlService/UrlServiceImpl.java
+++ evern/src/main/java/eus/evernature/evern/service/urlService/UrlServiceImpl.java
@@ -1,7 +1,15 @@
 package eus.evernature.evern.service.urlService;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
 
+import org.springframework.stereotype.Service;
+
+import lombok.NoArgsConstructor;
+
+@Service
+@NoArgsConstructor
+@Transactional
 public class UrlServiceImpl implements UrlService {
 
     @Override
