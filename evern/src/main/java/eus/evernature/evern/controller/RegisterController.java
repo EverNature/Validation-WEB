d393bd403e509e105ee432d89979b7672a85b2ab
Aritz Domaika <aritz.domaika@alumni.mondragon.edu>
2022-05-24 08:01:38 +0200

    register

---
+++ evern/src/main/java/eus/evernature/evern/controller/RegisterController.java
@@ -0,0 +1,17 @@
+package eus.evernature.evern.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+
+
+    @GetMapping
+    public String register() {
+        return "register";
+    }
+    
+}
