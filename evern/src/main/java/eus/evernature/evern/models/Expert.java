beaad7082bcc43bf17a5a40f1a8eb81f2a547d03
iarcos <izan.arcos@alumni.mondragon.edu>
2022-05-31 10:16:52 +0200

    jenkinsifle updated

--- evern/src/main/java/eus/evernature/evern/models/Expert.java
+++ evern/src/main/java/eus/evernature/evern/models/Expert.java
@@ -87,29 +87,4 @@ public class Expert {
     @UpdateTimestamp
     @Column(name = "fecha_modificacion")
     private Timestamp updatedTime;
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((username == null) ? 0 : username.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Expert other = (Expert) obj;
-        if (username == null) {
-            if (other.username != null)
-                return false;
-        } else if (!username.equals(other.username))
-            return false;
-        return true;
-    }
 }
